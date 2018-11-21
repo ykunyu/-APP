968f2b5382af67b3bfd211447fd754aa58b5e52e
yukunyu <y972275026.163.com>
2018-11-21 15:56:14 +0800

    初始化

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path=require('path');
+const webpack=require('webpack');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+module.exports={
+    entry:path.join(__dirname,"./src/main.js"),
+    output:{
+        path:path.join(__dirname,"./dist"),
+        filename:"bundle.js"
+    },
+    devServer:{
+        /*--open --port 3000 --contentBase src --hot*/
+        open:true,
+        port:3000,
+        hot:true
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template:path.join(__dirname,"./src/index.html"),
+            filename:'index.html'
+        }),
+        new VueLoaderPlugin()
+    ],
+    module:{
+        rules:[
+            {test:/\.css$/,use:['style-loader','css-loader']},
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+            {test:/\.scss$/,use:['style-loader','css-loader','sass-loader']},
+            {test:/\.(jpg|bmp|png|jpeg|gif)$/,use:['url-loader?limit=24857&name=[hash:8]-[name].[ext]']},
+            { test:/\.(eot|ttf|woff|woff2|svg)$/, loader: 'url-loader'},
+            { test:/\.js$/, loader: 'babel-loader',exclude:/node_modules/},
+            { test:/\.vue$/, loader: 'vue-loader'}
+
+        ]
+    },
+    resolve:{
+        alias:{
+            'vue$':'vue/dist/vue.js'
+        }
+    }
+}
